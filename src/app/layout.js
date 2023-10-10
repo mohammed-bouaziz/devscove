4d0b4e8d05762df79072e7eefd1411db7add802d
mohammed-bouaziz <63472622+mohammed-bouaziz@users.noreply.github.com>
2023-10-10 15:00:34 +0200

    fixed-mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${montserrat.variable} ${hind.variable}`}>
-      <body className="mx-auto max-w-2xl pt-10 pb-10 flex flex-col">
+      <body className="mx-auto max-w-2xl pt-10 pb-10 flex flex-col p-10">
         <Header />
         {children}
         <Footer />
